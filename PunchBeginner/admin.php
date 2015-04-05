6b60680ddf3f5f032703df7350befdff6916c8a0
atrotter <andrew.trotter@mail.utoronto.ca>
2015-04-05 17:51:32 -0400

    Added the admin file with one item, total count of funded projects.

--- PunchBeginner/admin.php
+++ PunchBeginner/admin.php
@@ -2,7 +2,24 @@
     require_once "Includes/session.php";
     require_once "Includes/connectDB.php";
     include("Includes/header.php");
+    
+    
+
+    
+?>
+<div id="main">
+    <div id="container">
+<?php
+    $result = mysql_query($databaseConnection,"SELECT count(*) FROM projects WHERE currentfunds >= goal");
+    if ($res = $result->fetch_assoc()){
+        echo "Total projects funded:";
+        echo $res;
+    }
+?>        
+    </div>
+</div>
+<?php
+    include ("Includes/closeDB.php");
+    include("Includes/footer.php");
 
 ?>
-
-
